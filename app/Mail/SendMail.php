61c439cc1dd84f46d14a3cc530cc3ce3db777aab
Aditya Nurdin <adityanurdin0@gmail.com>
2021-04-21 14:48:08 +0700

    delete client

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -30,7 +30,7 @@ class SendMail extends Mailable
     public function build()
     {
         return $this->from(\env('MAIL_FROM_ADDRESS'))
-        ->subject('Penawaran Baru '. $send['nama_perusahaan'])
+        ->subject('Penawaran Baru '. $this->send['nama_perusahaan'])
         ->view('emails.template_send_email')
         ->with('send', $this->send);
     }
